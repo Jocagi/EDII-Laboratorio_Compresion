611668e9c162dc04e8bde6d3dd56de53657c0658
JOSE CARLOS GIRON MARQUEZ <jcgironma@correo.url.edu.gt>
2019-09-12 17:51:49 -0600

    Code De-Janking

--- Laboratorio_Compresión/Controllers/HomeController.cs
+++ Laboratorio_Compresión/Controllers/HomeController.cs
@@ -13,9 +13,16 @@ namespace Laboratorio_Compresión.Controllers
     {
         //Lista de archivos comprimidos
         public static List<MisCompresiones> misCompresiones = new List<MisCompresiones>();
+        public static string directorioUploads;
+        public static string directorioHuffman;
+        public static string directorioHuffmanConfig;
 
         public ActionResult Index()
         {
+            directorioHuffman = Server.MapPath("~/Archivos/Huffman");
+            directorioUploads = Server.MapPath("~/Archivos/Uploads");
+            directorioHuffmanConfig = Server.MapPath("~/Archivos/HuffmanConfig");
+
             return View();
         }
 
@@ -27,7 +34,7 @@ namespace Laboratorio_Compresión.Controllers
             if (file != null && file.ContentLength > 0)
                 try
                 {
-                    string path = Path.Combine(Server.MapPath("~/Archivos"), Path.GetFileName(file.FileName));
+                    string path = Path.Combine(directorioUploads, Path.GetFileName(file.FileName));
 
                     if (System.IO.File.Exists(path))
                     {
@@ -37,7 +44,7 @@ namespace Laboratorio_Compresión.Controllers
                     file.SaveAs(path);
                     ViewBag.Message = "Carga Exitosa";
 
-                    comprimir(path);
+                    Huffman.comprimir(path);
                 }
                 catch (Exception ex)
                 {
@@ -49,110 +56,6 @@ namespace Laboratorio_Compresión.Controllers
             }
 
             return View();
-        }
-
-        public void comprimir(string path)
-        {
-
-            string Data = System.IO.File.ReadAllText(path); //To Do.. Sustituir por Bufffer
-            List<char> Caracteres = Data.ToList<char>();
-
-            Dictionary<char, int> dictionary = new Dictionary<char, int>();
-
-            //Leer caracteres y contarlos
-
-            foreach (var item in Caracteres)
-            {
-                if (!dictionary.Keys.Contains(item))
-                {
-                    dictionary.Add(item, 1);
-                }
-                else
-                {
-                    dictionary[item]++;
-                }
-            }
-
-            //Crear arbol Huffman
-            ArbolBinario arbol = new ArbolBinario(dictionary);
-            Dictionary<char, string> diccionario = arbol.obtenerDiccionarioBinario();
-            
-            //Leer archivo original y sustituir por codigos prefijo
-
-            string Data1 = System.IO.File.ReadAllText(path); //To Do.. Sustituir por Bufffer 
-            string textoBinario = "";
-
-
-            foreach (char caracter in Data1)
-            {
-                if (diccionario.ContainsKey(caracter))
-                {
-                    textoBinario += diccionario[caracter];
-                }
-                else
-                {
-                    throw new Exception("El diccionario no contiene el valor especificado");
-                }
-            }
-
-
-            //Escribir nuevo archivo
-
-            string nombreNuevoArchivo = Path.GetFileNameWithoutExtension(path) + ".huff";
-            string rutaComprimido = Path.Combine(Server.MapPath("~/Huffman"), nombreNuevoArchivo);
-
-            if (System.IO.File.Exists(rutaComprimido))
-            {
-                System.IO.File.Delete(rutaComprimido);
-            }
-            FileStream file = System.IO.File.Create(rutaComprimido);
-            file.Close();
-
-            //Construir texto huffman 
-            string Byte = ""; //Valor de 8 bits 
-            List<char> binario = textoBinario.ToArray().ToList(); //Arreglo de texto en binario
-            string textoComprimido = ""; //Resultado
-            bool completed = false;
-
-            while (!completed)
-            {
-                if (binario.Count > 0)
-                {
-                    //Construir byte
-                    Byte += binario[0];
-                    binario.RemoveAt(0); //Remover valor del arreglo
-
-                    if (Byte.Length == 8)
-                    {
-                        //To Do... Escribir en archivo directamente
-                        //Agregar Ascii al comprimido
-                        textoComprimido += Encoding.ASCII.GetString(new byte[] { Convert.ToByte(Convert.ToInt32(Byte, 2)) });
-                        Byte = "";
-                    }
-                }
-                else
-                {
-                    //Llenar el resto de espacio con 0's
-                    if (Byte != "")
-                    {
-                        while (Byte.Length != 8)
-                        {
-                            Byte += "0";
-                        }
-
-                        textoComprimido += Encoding.UTF8.GetString(new byte[] { Convert.ToByte(Convert.ToInt32(Byte, 2)) });
-                        Byte = "";
-                    }
-
-                    completed = true;
-                }
-            }
-
-            System.IO.File.WriteAllText(rutaComprimido, textoComprimido); //Escribir en archivo
-            
-            //Guardar configuracion para descomprimir
-
-            //To Do...
         }
 
         public ActionResult DownloadFile() //No se como sirve pero no tocar
---
+++ Laboratorio_Compresión/Huffman.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Laboratorio_Compresión.Models;
+using System.Text;
+using System.IO;
+
+using System.Web.Mvc;
+using Laboratorio_Compresión.Controllers;
+
+namespace Laboratorio_Compresión
+{
+    public static class Huffman
+    {
+
+        public static void comprimir(string path)
+        {
+
+            string Data = System.IO.File.ReadAllText(path); //To Do.. Sustituir por Bufffer
+            List<char> Caracteres = Data.ToList<char>();
+
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            #region Caracteres
+            //Leer caracteres y contarlos
+
+            foreach (var item in Caracteres)
+            {
+                if (!dictionary.Keys.Contains(item))
+                {
+                    dictionary.Add(item, 1);
+                }
+                else
+                {
+                    dictionary[item]++;
+                }
+            }
+            #endregion
+
+            #region Codigos_Prefijo
+            //Crear arbol Huffman
+            ArbolHuffman arbol = new ArbolHuffman(dictionary);
+            Dictionary<char, string> diccionario = arbol.obtenerDiccionarioBinario();
+
+            //Leer archivo original y sustituir por codigos prefijo
+
+            string Data1 = System.IO.File.ReadAllText(path); //To Do.. Sustituir por Bufffer 
+            string textoBinario = "";
+
+
+            foreach (char caracter in Data1)
+            {
+                if (diccionario.ContainsKey(caracter))
+                {
+                    textoBinario += diccionario[caracter];
+                }
+                else
+                {
+                    throw new Exception("El diccionario no contiene el valor especificado");
+                }
+            }
+
+            #endregion
+
+            #region Escritura
+            //Escribir nuevo archivo
+
+            string nombreNuevoArchivo = Path.GetFileNameWithoutExtension(path) + ".huff";
+            string rutaComprimido = Path.Combine(HomeController.directorioHuffman, nombreNuevoArchivo);
+
+            if (System.IO.File.Exists(rutaComprimido))
+            {
+                System.IO.File.Delete(rutaComprimido);
+            }
+            FileStream file = System.IO.File.Create(rutaComprimido);
+            file.Close();
+
+            //Construir texto huffman 
+            string Byte = ""; //Valor de 8 bits 
+            List<char> binario = textoBinario.ToArray().ToList(); //Arreglo de texto en binario
+            string textoComprimido = ""; //Resultado
+            bool completed = false;
+
+            while (!completed)
+            {
+                if (binario.Count > 0)
+                {
+                    //Construir byte
+                    Byte += binario[0];
+                    binario.RemoveAt(0); //Remover valor del arreglo
+
+                    if (Byte.Length == 8)
+                    {
+                        //To Do... Escribir en archivo directamente
+                        //Agregar Ascii al comprimido
+                        textoComprimido += Encoding.ASCII.GetString(new byte[] { Convert.ToByte(Convert.ToInt32(Byte, 2)) });
+                        Byte = "";
+                    }
+                }
+                else
+                {
+                    //Llenar el resto de espacio con 0's
+                    if (Byte != "")
+                    {
+                        while (Byte.Length != 8)
+                        {
+                            Byte += "0";
+                        }
+
+                        textoComprimido += Encoding.UTF8.GetString(new byte[] { Convert.ToByte(Convert.ToInt32(Byte, 2)) });
+                        Byte = "";
+                    }
+
+                    completed = true;
+                }
+            }
+
+            System.IO.File.WriteAllText(rutaComprimido, textoComprimido); //Escribir en archivo
+
+            //Guardar configuracion para descomprimir
+
+            string nombreArchivoConfig = Path.GetFileNameWithoutExtension(path) + ".config";
+            string rutaConfig = Path.Combine(HomeController.directorioHuffmanConfig, nombreNuevoArchivo);
+            
+            if (System.IO.File.Exists(rutaConfig))
+            {
+                System.IO.File.Delete(rutaConfig);
+            }
+            
+            FileStream fs = System.IO.File.Create(rutaConfig);
+            fs.Close();
+            
+            var csv = new StringBuilder();
+
+            string configuracion = string.Format("{0}", Path.GetFileName(path)); //Linea con informacion del archivo
+            csv.AppendLine(configuracion);
+
+            foreach (var item in diccionario)
+            {
+                //Lineas del diccionario
+                string newLine = string.Format("{0},{1}", item.Key, item.Value);
+                csv.AppendLine(newLine);
+            }
+            
+            //Escribir
+            System.IO.File.WriteAllText(rutaConfig, csv.ToString());
+
+            #endregion
+        }
+
+        public static void descomprimir(string path)
+        {
+
+        }
+    }
+}

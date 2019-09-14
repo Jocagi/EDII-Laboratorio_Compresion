64896a53766fd1af60addcd462db71fbd79d6e8c
genecc15 <genecc15@gmail.com>
2019-09-13 22:43:26 -0600

    Final Buffer

--- Laboratorio_Compresión/Controllers/HomeController.cs
+++ Laboratorio_Compresión/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Laboratorio_Compresión.Controllers
         [HttpPost]
         public ActionResult Index(HttpPostedFileBase archivo)
         {
-           // List<Frecuencias> frecuencia = new List<Frecuencias>();
+          
             string filePath = string.Empty;
             if (archivo != null)
             {
@@ -36,9 +36,6 @@ namespace Laboratorio_Compresión.Controllers
                 string extension = Path.GetExtension(archivo.FileName);
                 archivo.SaveAs(filePath);
                 string text = filePath;
-                
-               // WriteFile(text);
-               // readfile();
 
                 string csvData = System.IO.File.ReadAllText(filePath);
                 List<char> Caracteres = csvData.ToList<char>();
@@ -86,161 +83,17 @@ namespace Laboratorio_Compresión.Controllers
             }
             return View(Caracteres);
         }
-        class ByteGenerator
-        {
-            public static byte[] ConvertToBytes(string text)
-            {
-                return Encoding.ASCII.GetBytes(text);
-            }
-            public static string ConvertToString(byte[] bytes)
-            {
-                return Encoding.ASCII.GetString(bytes);
-            }
-            public static byte[] ConvertToBytes(char[] text)
-            {
-                return Encoding.ASCII.GetBytes(text);
-            }
-        }
       
-        public void leer(string text, string path)
-        {
-            List<char> tata = new List<char>();
-            int txt = Convert.ToInt32(text);
-            int bufferLength = txt;
-            var buffer = new byte[bufferLength];
-            using (var file = new FileStream(path, FileMode.Open))
-            {
-                using (var reader = new BinaryReader(file))
-                {
-                    while (reader.BaseStream.Position != reader.BaseStream.Length)
-                    {
-                        buffer = reader.ReadBytes(bufferLength);
-                        foreach (var item in buffer)
-                        {
-                            tata.Add((char)item);
-                        }
-                    }
-                }
-            }
-        }
-        #region buffer prueba
-        public static void Escritura(string text, string path)
-        {
-            List<char> tata = new List<char>();
-            int txt = Convert.ToInt32(text);
-            int bufferLength = txt;
-            var buffer = new byte[bufferLength];
-            using (var file = new FileStream(path, FileMode.Create))
-            {
-                using (var writer = new BinaryWriter(file))
-                {
-                    for(int i = 0; i<buffer.Length; i++)
-                    {
-                        buffer[i] = Convert.ToByte(100 + i);
-                    }
-                    writer.Write(buffer);
-                }
-            }
-
-        }
-        /*
-        static void WriteFile(string text)
-        {
-            var dir = Directory.GetCurrentDirectory();
-            var file = Path.Combine(dir, "File.dat");
-
-            try
-            {
-                FileStream fs = new FileStream(file, FileMode.Append, FileAccess.Write);
-                if (fs.CanWrite)
-                {
-                    //Aqui ya va por el ASCII
-                    byte[] buffer = Encoding.ASCII.GetBytes(text);
-                    fs.Write(buffer, 0, buffer.Length);
-                }
-                fs.Flush();
-                fs.Close();  
-            }
-            catch (Exception ex)
-            {
-                WriteLine(Environment.NewLine + ex.Message);
-            }
-        }
-       
-        static void readfile()
-        {
-            var dir = Directory.GetCurrentDirectory();
-            var file = Path.Combine(dir, "file.dat");
-            string text = String.Empty;
-
-            try
-            {
-                FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read);
-                byte[] buffer = new byte[1024];
-                int bytesread = fs.Read(buffer, 0, buffer.Length);              
-
-                text = Encoding.ASCII.GetString(buffer, 0, bytesread);
-
-                fs.Flush();
-                fs.Close();
-            }
-
-            catch (Exception ex)
-            {
-                WriteLine(Environment.NewLine + ex.Message);
-            }
-
-            WriteLine();
-            WriteLine("El texto es: " + Environment.NewLine + text);
-        }*/
-
-        #endregion
         public ActionResult contar()
         {
             char carac;
-            int frec;
             foreach (var item in Caracteres.GroupBy(x => x))
                 carac = item.Key;
 
 
             return View();
         }
-        #region prueba diccionario
-        /*  public ActionResult Diccionario()
-          {
-              var res = new Dictionary<Char, int>();
-              foreach(Char item in Caracteres)
-              {
-                  if(!res.ContainsKey(item))
-                  {
-                      res.Add(item, new Archivo{caracter=item.)
-                  }
-              }
-          }*/
-
-        /*[HttpPost]
-         public ActionResult Index(HttpPostedFileBase file)
-         {
-             //Subir archivos al servidor
-
-             if (file != null && file.ContentLength > 0)
-                 try
-                 {
-                     string path = Path.Combine(Server.MapPath("~/Uploads"), Path.GetFileName(file.FileName));
-                     file.SaveAs(path);
-                     ViewBag.Message = "File uploaded successfully";
-                 }
-                 catch (Exception ex)
-                 {
-                     ViewBag.Message = "ERROR:" + ex.Message.ToString();
-                 }
-             else
-             {
-                 ViewBag.Message = "You have not specified a file.";
-             }
-             return View();
-         }*/
-        #endregion
+ 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";

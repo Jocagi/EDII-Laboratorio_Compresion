c475f5ae0fe60c3124bbe8840f9ac84962e01250
JOSE CARLOS GIRON MARQUEZ <jcgironma@correo.url.edu.gt>
2019-09-15 16:50:59 -0600

    Preparacion para LZW

---
+++ Laboratorio_Compresión/LZW.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio_Compresión
+{
+    public class LZW
+    {
+        public static void comprimir(string path)
+        {
+
+        }
+
+        public static void descomprimir(string path)
+        {
+
+        }
+    }
+}
--- Laboratorio_Compresión/Models/Archivo.cs
+++ Laboratorio_Compresión/Models/Archivo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -9,5 +10,28 @@ namespace Laboratorio_Compresión.Models
     {
         public string caracter { get; set; }
         public int frecuencia { get; set;}
+
+        public static void crearArchivo(string path)
+        {
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+
+            FileStream fs = System.IO.File.Create(path);
+            fs.Close();
+        }
+
+        public static void escribirEnArchivo(string path, string text)
+        {
+            if (File.Exists(path))
+            {
+                File.AppendAllText(path, text);
+            }
+            else
+            {
+                throw new Exception("El Archivo no existe");
+            }
+        }
     }
 }

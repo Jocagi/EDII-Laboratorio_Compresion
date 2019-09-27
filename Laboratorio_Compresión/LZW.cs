05aabd42107813b22d1f0cc8131083acded1e789
genecc15 <genecc15@gmail.com>
2019-09-26 20:34:10 -0600

    Concatenar

--- Laboratorio_Compresión/LZW.cs
+++ Laboratorio_Compresión/LZW.cs
@@ -3,55 +3,113 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Text;
+using Laboratorio_Compresión.Controllers;
 
 namespace Laboratorio_Compresión
 {
     public class LZW
     {
-        
-        public Dictionary<int, string> Comprimir(string path, ref List<int> indices)
+        #region prueba1
+        /* public Dictionary<int, string> Comprimir(string path, ref List<int> indices)
+         {
+             Dictionary<int, string> prefijos = new Dictionary<int, string>();
+
+             for (int i = 0; i < 256; i++)
+                 prefijos.Add(i, new string((char)i, 1));
+             char c = '\0';
+             int index = 1, n = path.Length, nextkey = 256;
+             string s = new string(path[0], 1), sc = string.Empty;
+
+             while (index<n)
+             {
+                 c = path[index++];
+                 sc = s + c;
+
+                 if (prefijos.ContainsValue(sc))
+                     s = sc;
+
+                 else
+                 {
+                     foreach(KeyValuePair<int, string> item in prefijos)
+                     {
+                         if (item.Value==s)
+                         {
+                             indices.Add(item.Key);
+                                 break;
+                         }
+                     }
+
+                     prefijos.Add(nextkey++, sc);
+                     s = new string(c, 1);
+                 }
+             }
+
+             foreach(KeyValuePair<int, string>item in prefijos)
+             {
+                 if(item.Value==s)
+                 {
+                     indices.Add(item.Key);
+                     break;
+                 }
+             }
+             return prefijos;
+         }*/
+        #endregion
+
+        public static List<int> comprimir(string path)
         {
-            Dictionary<int, string> prefijos = new Dictionary<int, string>();
-
+            string Data = System.IO.File.ReadAllText(path, Encoding.Default); //buffer
+            List<char> Caracteres = Data.ToList<char>();
+            //Crea diccionario
+            Dictionary<string, int> diccionario = new Dictionary<string, int>(); 
             for (int i = 0; i < 256; i++)
-                prefijos.Add(i, new string((char)i, 1));
-            char c = '\0';
-            int index = 1, n = path.Length, nextkey = 256;
-            string s = new string(path[0], 1), sc = string.Empty;
+                diccionario.Add(((char)i).ToString(), i);
 
-            while (index<n)
+            //Empezar concatenar
+            string c = string.Empty;
+            List<int> comprimir = new List<int>();
+
+            //Lee desde el path
+            foreach(char t in path)
             {
-                c = path[index++];
-                sc = s + c;
-
-                if (prefijos.ContainsValue(sc))
-                    s = sc;
-
+                string ct = c + t;
+                if (diccionario.ContainsKey(ct))
+                {
+                    c = ct;
+                }
                 else
                 {
-                    foreach(KeyValuePair<int, string> item in prefijos)
-                    {
-                        if (item.Value==s)
-                        {
-                            indices.Add(item.Key);
-                                break;
-                        }
-                    }
-
-                    prefijos.Add(nextkey++, sc);
-                    s = new string(c, 1);
+                    //sacarlo
+                    comprimir.Add(diccionario[c]);
+                    //Aqui ya lo concatena y lo agrega
+                    diccionario.Add(ct, diccionario.Count);
+                    c = t.ToString();
                 }
             }
 
-            foreach(KeyValuePair<int, string>item in prefijos)
+            if (!string.IsNullOrEmpty(c))
+                comprimir.Add(diccionario[c]);
+
+            return comprimir;
+            //char[] Carac = Caracteres.ToArray();
+           // Dictionary<char, int> diccionario = new Dictionary<char, int>();
+            /*foreach (var item in Caracteres)
             {
-                if(item.Value==s)
+                if(!diccionario.Keys.Contains(item))
                 {
-                    indices.Add(item.Key);
-                    break;
+                    diccionario.Add(item, 1);
                 }
-            }
-            return prefijos;
+                else if(diccionario.Keys.Contains(item))
+                {
+                  
+                    for (int i = 0; i < Carac.Length; i++)
+                    {
+                        string charsStr = new string(Carac);
+                        diccionario.Add(item+1,  1);
+                    } 
+                }
+                
+            }*/
         }
 
         public static void descomprimir(string path)

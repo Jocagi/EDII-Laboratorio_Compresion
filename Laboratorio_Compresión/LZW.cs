5498ba93a6598f045cf435f1578b55601d838c3a
JOSE CARLOS GIRON MARQUEZ <jcgironma@correo.url.edu.gt>
2019-09-27 18:02:31 -0600

    Bufffer LZW

--- Laboratorio_Compresión/LZW.cs
+++ Laboratorio_Compresión/LZW.cs
@@ -16,7 +16,7 @@ namespace Laboratorio_Compresión
             string rutaComprimido; 
             Dictionary<char, int> dictionary;
             int dictionaryLenght = 0;
-            int byteLenght = 0;
+            int byteLenght = 8;
 
             #endregion
 
@@ -28,32 +28,14 @@ namespace Laboratorio_Compresión
             
             #endregion
 
-            //Leer posibles caracteres del archivo
+            //Crear dicionario
             #region Caracteres
 
             dictionary = Lectura.obtenerDiccionarioFrecuencias(path);
             dictionaryLenght = dictionary.Count;
 
             #endregion
-
-            //Añadir metadata del archivo
-            #region Configuracion
-            //First byte: byteLenght, Second byte: dictionary lenght, then complete dictionary
-
-            List<byte> metadata = new List<byte> {Convert.ToByte(byteLenght), Convert.ToByte(dictionaryLenght)};
             
-            //Diccionario
-            foreach (var item in dictionary)
-            {
-                metadata.Add(Convert.ToByte(item.Key));
-            }
-
-            byte[] metadataArray = metadata.ToArray();
-
-            Lectura.InsertData(rutaComprimido, 0, metadataArray);
-
-            #endregion
-
             //Analizar texto, creando diccionario y escribiendo en archivo
             #region Algoritmo
 
@@ -63,7 +45,7 @@ namespace Laboratorio_Compresión
             #endregion
 
             //Obtener codigo mas grande y añadirlo a la metadata
-            //ToDo...
+            
 
         }
 
@@ -71,31 +53,16 @@ namespace Laboratorio_Compresión
         {
             int bufferLength = 1024;
 
-            int byteLenght;
-            int dictionaryLenght;
+            int byteLenght = 8;
+            int byteLenghtMax;
             Dictionary<int, char> dictionary = new Dictionary<int, char>();
-            
-            string current = "";
-            string previous = "";
-            string newItem = "";
             
             using (var file = new FileStream(path, FileMode.Open))
             {
                 using (var reader = new BinaryReader(file))
                 {
-                    //leer tamaño de los bytes
-                    byteLenght = Convert.ToInt16(reader.ReadByte());
-                    // leer tamaño del diccionario
-                    dictionaryLenght = Convert.ToInt16(reader.ReadByte());
-
                     //Definir diccionario
-                    int position = 1;
-
-                    while (position <= dictionaryLenght)
-                    {
-                        dictionary.Add(position, (char)reader.ReadByte());
-                        position++;
-                    }
+                    
 
                     //Buffer para descomprimir
                     while (reader.BaseStream.Position != reader.BaseStream.Length)
